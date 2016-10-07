28d408052c36dbe08f224ada879f36c6aad59f0f
walkermannu1 <2469377884@qq.com>
2016-10-07 22:03:25 +0800

    Show location I was stand on.

--- app/src/main/java/com/example/bi/train/MapActivity.java
+++ app/src/main/java/com/example/bi/train/MapActivity.java
@@ -1,7 +1,15 @@
 package com.example.bi.train;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
@@ -9,11 +17,20 @@ import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+import java.util.List;
 
 public class MapActivity extends Activity {
     public LocationClient mLocationClient = null;
     public BDLocationListener myListener = new MyLocationListener();
+    private LocationManager locationManager;
+    private String provider;
+    private boolean isFirstLocate = true;
 
     BitmapDescriptor mapMaker;
     MapView mMapView;
@@ -25,8 +42,9 @@ public class MapActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        mMapView = (MapView)findViewById(R.id.bmapView);
+        mMapView = (MapView) findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
+        setLovationManager();
         mBaiduMap.setMyLocationEnabled(true);
 
         mLocationClient = new LocationClient(getApplicationContext());
@@ -34,6 +52,82 @@ public class MapActivity extends Activity {
         initLocation();
         mLocationClient.start();
     }
+
+    private void setLovationManager() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        List<String> providerList = locationManager.getProviders(true);
+        if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+            provider = LocationManager.GPS_PROVIDER;
+        } else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+            provider = LocationManager.NETWORK_PROVIDER;
+        } else {
+            //当前没有可用的位置提供器时，弹出Toast提示
+            Toast.makeText(this, "没有可用的位置提供器", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            navigateTo(location);
+        }
+
+        locationManager.requestLocationUpdates(provider, 5000, 5, locationListener);
+    }
+
+    private void navigateTo(Location location) {
+        //如果是第一次创建，就获取位置信息并且将地图移到当前位置
+        //为防止地图被反复移动，所以就只在第一次创建时执行
+        if (isFirstLocate) {
+            //LatLng对象主要用来存放经纬度
+            //zoomTo是用来设置百度地图的缩放级别，范围为3~19，数值越大越精确
+            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+            MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(ll);
+            mBaiduMap.animateMapStatus(update);
+            update = MapStatusUpdateFactory.zoomTo(16f);
+            mBaiduMap.animateMapStatus(update);
+            isFirstLocate = false;
+        }
+
+        //封装设备当前位置并且显示在地图上
+        //由于设备在地图上显示的位置会根据我们当前位置而改变，所以写到if外面
+        MyLocationData.Builder locationBuilder = new MyLocationData.Builder();
+        locationBuilder.latitude(location.getLatitude());
+        locationBuilder.longitude(location.getLongitude());
+        MyLocationData locationData = locationBuilder.build();
+        mBaiduMap.setMyLocationData(locationData);
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if (locationManager != null)
+                navigateTo(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
     private void initLocation() {
         LocationClientOption option = new LocationClientOption();
@@ -66,6 +160,19 @@ public class MapActivity extends Activity {
     @Override
     protected void onDestroy() {
         mLocationClient.stop();
+        if (locationManager != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.removeUpdates(locationListener);
+        }
         mBaiduMap.setMyLocationEnabled(false);
         mMapView.onDestroy();
         mMapView = null;
